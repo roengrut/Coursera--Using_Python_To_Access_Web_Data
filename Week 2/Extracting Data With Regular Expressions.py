5528a1f7236ce23ec4346095fbbb8bf599896fdf
roengrut <36532355+roengrut@users.noreply.github.com>
2022-11-21 22:49:22 +0700

    Update Extracting Data With Regular Expressions.py

--- Week 2/Extracting Data With Regular Expressions.py
+++ Week 2/Extracting Data With Regular Expressions.py
@@ -4,16 +4,16 @@ You will extractall the numbers in the file and compute the sum of the numbers.
 '''
 import re
 
-fname = ('regex_sum_42.txt')
-#fname = ('regex_sum_1683143.txt')
+#fname = ('regex_sum_42.txt')
+fname = ('regex_sum_1683143.txt')
 handle = open(fname)
 sum=0
 count = 0
 for line in handle:
 	f = re.findall('[0-9]+',line)
 	for num in f:
-		if num >= [0]:			
+		if int(num) >= 0:
 			count += 1
 			sum += int(num)
-		
+
 print('There are ',count,'values with the sum = ', sum)
